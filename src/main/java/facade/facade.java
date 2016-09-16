049986281d5b1bbf76750a541fe26d87f73c06ba
matyasfeher <fehermatyas17@gmail.com>
2016-09-16 15:26:14 +0200

    2

--- src/main/java/facade/facade.java
+++ src/main/java/facade/facade.java
@@ -1,11 +1,34 @@
 package facade;
 
+import entity.Person;
+import java.util.Date;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.EntityManager;
+
 /**
  *
  * @author Acer
  */
 public class facade {
-    
-    
-    
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("PU");
+
+    public void addPerson(String fname, String lname, Date bd, int age, boolean isMarried) {
+        EntityManager em = emf.createEntityManager();
+        Person p = new Person(fname, lname, bd, age, isMarried);
+        try {
+            em.getTransaction().begin();
+            em.persist(p);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+
+    }
+
 }
